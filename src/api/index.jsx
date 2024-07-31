bdf46a1af6ff471f5a96779e2c8827320f5f5177
Gangatharan k <154905189+Ganga-tharank@users.noreply.github.com>
2024-07-31 07:41:37 +0530

    Update index.jsx

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://capstone-projec-be-8.onrender.com/api",
+  baseURL: "https://capstone-projec-be-12.onrender.com/api",
 });
 export default api;
